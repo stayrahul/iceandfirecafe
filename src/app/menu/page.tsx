5aa3835b1524e74209af65000fe0dcbfabe567b5
Rahul <rahul@Rahuls-MacBook-Air.local>
2026-07-16 14:55:48 +0545

    Fix: Add missing content to menu page to resolve Vercel build error

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,9 @@
+import BentoMenu from '../../components/home/BentoMenu';
+
+export default function MenuPage() {
+  return (
+    <div className="w-full bg-[#FDFBF7] pt-24 min-h-screen">
+      <BentoMenu />
+    </div>
+  );
+}
